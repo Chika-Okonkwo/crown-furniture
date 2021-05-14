5d54ab33fa91c70c28e2dfd0b6b534040a1fec9e
Chika Okonkwo <cokonkwo@eminenttechnology.com>
2021-05-14 17:52:23 +0100

    stylying directory menu items

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
 
 import sections from "./directory.data";
 import "./directory.styles.scss";
 
-const Directory = () => {
-  return (
-    <div className="directory-menu">
-      {sections.map(({ id, title, imageUrl, size }) => (
-        <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
-      ))}
-    </div>
-  );
-};
+export class Directory extends Component {
+  componentDidMount() {
+    this.state = sections;
+  }
+
+  render() {
+    return (
+      <div className="directory-menu">
+        {this.state.map(({ id, title, imageUrl, size }) => (
+          <MenuItem title={title} key={id} imageUrl={imageUrl} size={size} />
+        ))}
+      </div>
+    );
+  }
+}
 
 export default Directory;
